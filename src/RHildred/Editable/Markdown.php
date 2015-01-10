c8b856b89e1df28b24c4657531349037e942bd26
Rich Hildred <rhildred@gmail.com>
2015-01-10 16:42:35 -0500

    going to run a test against github

---
+++ src/RHildred/Editable/Markdown.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace RHildred/Editable;
+use \Michelf\MarkdownExtra;
+
+class Markdown
+{
+    public static function render($sFname){
+        echo '<div class="editable" id="' . str_replace('.', '_', $sFname) . '">' . MarkdownExtra::defaultTransform(file_get_contents($sFname)) . "</div>";
+
+    }
+}
