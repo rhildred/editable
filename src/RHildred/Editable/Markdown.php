5aedfa734d295dbaff3787fdae23f37a69523ec0
Rich Hildred <rhildred@gmail.com>
2015-01-10 22:35:08 -0500

    added a new method to save markdown

--- src/RHildred/Editable/Markdown.php
+++ src/RHildred/Editable/Markdown.php
@@ -3,12 +3,40 @@
 namespace RHildred\Editable;
 
 
-use \Michelf\MarkdownExtra;
+use \Michelf\MarkdownExtra, \kbjr\Git;
 
 class Markdown
 {
     public static function render($sFname){
         echo '<div class="editable" id="' . str_replace('.', '_', $sFname) . '">' . MarkdownExtra::defaultTransform(file_get_contents($sFname)) . "</div>";
+
+    }
+
+    public static function save($sId){
+        if(!isset($_SESSION["currentuser"])) throw new Exception('no user logged in');
+
+        //convert to html
+        $sMarkdown = $request->parameters["sValue"];
+        $sHtml = MarkdownExtra::defaultTransform($sMarkdown);
+
+        //save the file
+        $sFname = str_replace('_', '.', $sId);
+        $sDir = __DIR__ . '/../../../../../../public/';
+        file_put_contents($sDir . $sFname, $sMarkdown);
+
+        //delete refering file from cache
+        $sReferer = !empty($_SERVER['HTTP_REFERER'])? basename($_SERVER['HTTP_REFERER']):"index";
+        if($sReferer == "www" || $sReferer == "" || $sReferer == "salesucation.com") $sReferer = "index";
+        $sIndex = $sDir . $sReferer . "." . date("Y") . ".html";
+        if(file_exists($sIndex))unlink($sIndex);
+
+        // now we need to commit the file
+        putenv('DYLD_LIBRARY_PATH=/usr/lib/:$DYLD_LIBRARY_PATH');
+        $repo = Git::open(__DIR__ . '/../../../../../..');  // -or- Git::create('/path/to/repo')
+        $repo->add("public/" . $sFname);
+        $repo->commit('web commit from ' . $_SESSION["currentuser"]->name);
+
+        echo $sHtml;
 
     }
 }
